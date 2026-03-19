ce8d1d0c5f022a05cde3cc995578bd4b8811d04a
hongtao_luo <luohongtao621@hotmail.com>
2026-03-19 15:25:26 +0800

    model type

---
+++ YoloSharpOnnx/ModelType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace YoloSharpOnnx
+{
+    public enum ModelType
+    {
+        [EnumMember(Value = "classify")]
+        Classification,
+
+        [EnumMember(Value = "detect")]
+        ObjectDetection,
+
+        [EnumMember(Value = "obb")]
+        ObbDetection,
+
+        [EnumMember(Value = "segment")]
+        Segmentation,
+
+        [EnumMember(Value = "pose")]
+        PoseEstimation
+    }
+}
