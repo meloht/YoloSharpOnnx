16bb5f54965151db428d2633f5f5e158a72fc0f7
hongtao_luo <luohongtao621@hotmail.com>
2026-03-23 17:58:34 +0800

    SpeedResult

--- YoloSharpOnnx/DetectionBoxesExtensions.cs
+++ YoloSharpOnnx/DetectionBoxesExtensions.cs
@@ -7,11 +7,11 @@ namespace YoloSharpOnnx
 {
     public static class DetectionBoxesExtensions
     {
-        public static string Summary(this IEnumerable<DetectionResult> boxes)
+        public static string Summary(this ICollection<DetectionResult> boxes)
         {
-            if (boxes == null || boxes.Count() == 0)
+            if (boxes == null || boxes.Count == 0)
                 return string.Empty;
-
+     
             var dict = boxes.GroupBy(p => p.ClassName).Select(p => $"{p.Count()} {p.Key}").ToList();
             string confs = string.Join(", ", boxes.Select(p => Math.Round(p.Confidence, 2)));
             return $"{string.Join(", ", dict)} [{confs}]";
