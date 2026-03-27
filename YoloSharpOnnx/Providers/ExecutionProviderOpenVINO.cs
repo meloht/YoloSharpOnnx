ab3644c623358872694b59a1c5ae8ba5709223d5
luohongtao <luohongtao621@hotmail.com>
2026-03-27 16:10:01 +0800

    intel OpenVINO

---
+++ YoloSharpOnnx/Providers/ExecutionProviderOpenVINO.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ML.OnnxRuntime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoloSharpOnnx.Inference;
+using YoloSharpOnnx.Models;
+
+namespace YoloSharpOnnx.Providers
+{
+    public class ExecutionProviderOpenVINO : ExecutionProvider, IExecutionProvider
+    {
+        private IntelDeviceType _intelDeviceType;
+
+        public ExecutionProviderOpenVINO(string modelPath, IntelDeviceType intelDeviceType) : base(modelPath)
+        {
+            _intelDeviceType = intelDeviceType;
+        }
+
+        public IYoloDetect CreateYoloDetect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IYoloDetect GetYoloDetector(InferenceSession session, SessionOptions options, IPostprocess postprocess, OnnxModel onnxModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
