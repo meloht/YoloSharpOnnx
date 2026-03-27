171b53c68bc326cf96c2dc7c70e28a78105a1fe3
hongtao_luo <luohongtao621@hotmail.com>
2026-03-27 09:10:37 +0800

    result

--- YoloSharpOnnx/DataResult/DetectionBatchResult.cs
+++ YoloSharpOnnx/DataResult/DetectionBatchResult.cs
@@ -4,6 +4,26 @@ using System.Text;
 
 namespace YoloSharpOnnx.DataResult
 {
-    public record DetectionBatchResult(string ImagePath, List<DetectionResult> Results);
-   
+    public class DetectionBatchResult
+    {
+        public string ImagePath { get; set; }
+
+        public List<DetectionResult> Results { get; set; }
+
+        public DetectionBatchResult(string imagePath, List<DetectionResult> results)
+        {
+            this.ImagePath = imagePath;
+            this.Results = results;
+        }
+
+        public override string ToString()
+        {
+            return $"Image:{Path.GetFileName(ImagePath)} Result:{YoloUtils.GetResult(Results)}";
+        }
+
+
+    }
+
+
+
 }
