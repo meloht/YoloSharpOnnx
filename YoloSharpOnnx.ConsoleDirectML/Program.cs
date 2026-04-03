4e38ff40c8131179df01c3f397667935edc5b716
luohongtao <luohongtao621@hotmail.com>
2026-04-03 20:17:24 +0800

    preprocessing performance optimization

--- YoloSharpOnnx.ConsoleDirectML/Program.cs
+++ YoloSharpOnnx.ConsoleDirectML/Program.cs
@@ -8,9 +8,9 @@ namespace YoloSharpOnnx.ConsoleDirectML
 {
     internal class Program
     {
-        static int _deviceId = 1;
+        static int _deviceId = 0;
         static string modelPath = @"D:\code\model\best.onnx";
-        static string dir = @"D:\code\model\TestImages_300";
+        static string dir = @"D:\code\model\TestImages";
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
