68e8c7b4511eb4d261b95c0e1fe7dd7367cb3a5c
luohongtao <luohongtao621@hotmail.com>
2026-07-08 12:43:40 +0800

    update package

--- YoloSharpOnnx.ConsoleDirectML/Program.cs
+++ YoloSharpOnnx.ConsoleDirectML/Program.cs
@@ -13,23 +13,23 @@ namespace YoloSharpOnnx.ConsoleDirectML
 {
     internal class Program
     {
-        static int _deviceId = 1;
-        static string modelPath = @"D:\code\model\best.onnx";
-        static string dir = @"D:\code\model\TestImages";
+        static int _deviceId = 0;
+        static string modelPath = @"C:\code\model\best.onnx";
+        static string dir = @"C:\code\model\TestImages";
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
 
             //TestChannel();
 
-            //TestBatchInfer();
+            TestBatchInfer();
             //TestBatchInferObb();
             // _=TestBatchInferForeachObb();
             //TestBatchInferSeg();
             //_ = TestBatchInferForeachSegAsync();
             //TestInferSeg();
             // _ = TestBatchForeachInfer();
-            TestInferPerf();
+            //TestInferPerf();
             // _ = TestInferBatchAsync();
             //TestInferCls();
             //TestInfer();
